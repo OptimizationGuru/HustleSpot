ec57f418330d931b1211f693d631451ecd83b09f
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-10-30 09:15:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // This targets all JavaScript and TypeScript files in src
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
