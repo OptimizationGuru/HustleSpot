baa178a2e499713b724766b3d233a5a39793b04a
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-11-02 10:52:10 +0530

    feat:responsive theme

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -34,7 +34,7 @@ const TaskList: React.FC<TaskListProps> = ({ onCreateTaskClick }) => {
   const [isDefaultView, setIsDefaultView] = useState(true);
   const [currentStatus, setCurrentStatus] = useState<TaskStatus | null>(null);
   const [welcome, setWelcome] = useState<boolean>(false);
-  const [isSearchKeyOn, setIsSearchKeyOn] = useState<boolean>(false);
+
   const { searchKey } = useSelector((store: RootState) => store.task);
 
   useEffect(() => {
@@ -64,10 +64,8 @@ const TaskList: React.FC<TaskListProps> = ({ onCreateTaskClick }) => {
       setActiveTaskList(filteredTasks);
       setIsSortActive(false);
       setIsFiltered(true);
-      if (tasks.length > 0 && activeTaskList?.length === 0)
-        setIsSearchKeyOn(true);
     },
-    [tasks, activeTaskList?.length]
+    [tasks]
   );
 
   useEffect(() => {
@@ -130,125 +128,125 @@ const TaskList: React.FC<TaskListProps> = ({ onCreateTaskClick }) => {
         <WelcomeCard onCreateTaskClick={onCreateTaskClick} />
       </div>
     );
-  } else
-    return (
-      <div
-        className="w-screen border-white border-t flex flex-col items-center md:justify-center gap-6 sm:gap-8 mt-28 px-2   sm:px-4 lg:px-8 bg-gradient-to-r from-gray-950 via-gray-900 to-black
+  }
+  return (
+    <div
+      className="w-screen border-white border-t flex flex-col items-center md:justify-center gap-6 sm:gap-8 mt-28 px-2   sm:px-4 lg:px-8 bg-gradient-to-r from-gray-950 via-gray-900 to-black
 
  text-gray-200"
+    >
+      <div className="flex flex-col items-center justify-center w-full md:ml-12">
+        <TaskListHeader onSelect={FilterTaskbyStatus} onSort={SortTasks} />
+      </div>
+      <div
+        className={`w-full max-w-6xl ${
+          isSortActive || isFiltered
+            ? 'flex flex-wrap justify-center gap-4 sm:gap-6'
+            : 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6'
+        }`}
       >
-        <div className="flex flex-col items-center justify-center w-full md:ml-12">
-          <TaskListHeader onSelect={FilterTaskbyStatus} onSort={SortTasks} />
-        </div>
-        <div
-          className={`w-full max-w-6xl ${
-            isSortActive || isFiltered
-              ? 'flex flex-wrap justify-center gap-4 sm:gap-6'
-              : 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6'
-          }`}
-        >
-          {isSortActive || isFiltered ? (
-            displayTasks.length > 0 ? (
-              displayTasks.map((task: Task) => (
-                <TaskCard
-                  key={task.id}
-                  taskDetails={task}
-                  onUpdate={updateTaskInList}
-                  className="bg-gray-800 border border-gray-700 text-gray-200"
-                />
-              ))
-            ) : (
-              <div className="flex flex-col items-center justify-center w-full p-4 border border-gray-700 rounded-lg shadow-md bg-gray-800 text-gray-300">
-                <NoTasksCard
-                  onCreateTaskClick={onCreateTaskClick}
-                  msg={taskNotFoundMsg}
-                />
-              </div>
-            )
+        {isSortActive || isFiltered ? (
+          displayTasks.length > 0 ? (
+            displayTasks.map((task: Task) => (
+              <TaskCard
+                key={task.id}
+                taskDetails={task}
+                onUpdate={updateTaskInList}
+                className="bg-gray-800 border border-gray-700 text-gray-200"
+              />
+            ))
           ) : (
-            <>
-              <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
-                <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text  bg-blue-400  shadow-lg">
-                  {taskYesterday}
-                </p>
+            <div className="flex flex-col items-center justify-center w-full p-4 border border-gray-700 rounded-lg shadow-md bg-gray-800 text-gray-300">
+              <NoTasksCard
+                onCreateTaskClick={onCreateTaskClick}
+                msg={taskNotFoundMsg}
+              />
+            </div>
+          )
+        ) : (
+          <>
+            <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
+              <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text  bg-blue-400  shadow-lg">
+                {taskYesterday}
+              </p>
 
-                <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
-                  {filterTasksByDefaultDates(activeTaskList).tasksYesterday
-                    .length === 0 ? (
-                    <NoTasksCard
-                      onCreateTaskClick={onCreateTaskClick}
-                      msg={taskYesterdayMsg}
-                    />
-                  ) : (
-                    filterTasksByDefaultDates(
-                      activeTaskList
-                    ).tasksYesterday.map((task: Task) => (
+              <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
+                {filterTasksByDefaultDates(activeTaskList).tasksYesterday
+                  .length === 0 ? (
+                  <NoTasksCard
+                    onCreateTaskClick={onCreateTaskClick}
+                    msg={taskYesterdayMsg}
+                  />
+                ) : (
+                  filterTasksByDefaultDates(activeTaskList).tasksYesterday.map(
+                    (task: Task) => (
                       <TaskCard
                         key={task.id}
                         taskDetails={task}
                         onUpdate={updateTaskInList}
                         className="bg-gray-800 border border-gray-700 text-gray-200"
                       />
-                    ))
-                  )}
-                </div>
+                    )
+                  )
+                )}
               </div>
-              <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
-                <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text bg-blue-400  shadow-lg">
-                  {taskToday}
-                </p>
+            </div>
+            <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
+              <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text bg-blue-400  shadow-lg">
+                {taskToday}
+              </p>
 
-                <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
-                  {filterTasksByDefaultDates(activeTaskList).tasksToday
-                    .length === 0 ? (
-                    <NoTasksCard
-                      onCreateTaskClick={onCreateTaskClick}
-                      msg={taskTodayMsg}
-                    />
-                  ) : (
-                    filterTasksByDefaultDates(activeTaskList).tasksToday.map(
-                      (task: Task) => (
-                        <TaskCard
-                          key={task.id}
-                          taskDetails={task}
-                          onUpdate={updateTaskInList}
-                          className="bg-gray-800 border border-gray-700 text-gray-200"
-                        />
-                      )
+              <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
+                {filterTasksByDefaultDates(activeTaskList).tasksToday.length ===
+                0 ? (
+                  <NoTasksCard
+                    onCreateTaskClick={onCreateTaskClick}
+                    msg={taskTodayMsg}
+                  />
+                ) : (
+                  filterTasksByDefaultDates(activeTaskList).tasksToday.map(
+                    (task: Task) => (
+                      <TaskCard
+                        key={task.id}
+                        taskDetails={task}
+                        onUpdate={updateTaskInList}
+                        className="bg-gray-800 border border-gray-700 text-gray-200"
+                      />
                     )
-                  )}
-                </div>
+                  )
+                )}
               </div>
-              <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
-                <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text bg-blue-400   shadow-lg">
-                  {taskTomorrow}
-                </p>
-                <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
-                  {filterTasksByDefaultDates(activeTaskList).tasksTomorrow
-                    .length === 0 ? (
-                    <NoTasksCard
-                      onCreateTaskClick={onCreateTaskClick}
-                      msg={taskTomorrowMsg}
-                    />
-                  ) : (
-                    filterTasksByDefaultDates(activeTaskList).tasksTomorrow.map(
-                      (task: Task) => (
-                        <TaskCard
-                          key={task.id}
-                          taskDetails={task}
-                          onUpdate={updateTaskInList}
-                          className="bg-gray-800 border border-gray-700 text-gray-200"
-                        />
-                      )
+            </div>
+            <div className="flex flex-col gap-2 sm:gap-4 items-center justify-center w-full text-gray-300">
+              <p className="text-lg sm:text-xl font-bold text-transparent bg-clip-text bg-blue-400   shadow-lg">
+                {taskTomorrow}
+              </p>
+              <div className="flex flex-col flex-wrap items-center justify-center gap-4 sm:gap-6 w-full">
+                {filterTasksByDefaultDates(activeTaskList).tasksTomorrow
+                  .length === 0 ? (
+                  <NoTasksCard
+                    onCreateTaskClick={onCreateTaskClick}
+                    msg={taskTomorrowMsg}
+                  />
+                ) : (
+                  filterTasksByDefaultDates(activeTaskList).tasksTomorrow.map(
+                    (task: Task) => (
+                      <TaskCard
+                        key={task.id}
+                        taskDetails={task}
+                        onUpdate={updateTaskInList}
+                        className="bg-gray-800 border border-gray-700 text-gray-200"
+                      />
                     )
-                  )}
-                </div>
+                  )
+                )}
               </div>
-            </>
-          )}
-        </div>
+            </div>
+          </>
+        )}
       </div>
-    );
+    </div>
+  );
 };
 
 export default TaskList;
