378ed0d0c94c0e5b840f12a8b263084bbd6abd57
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-11-02 11:47:03 +0530

    feat:responsive theme

--- src/components/CreateTask/Sidebar.tsx
+++ src/components/CreateTask/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, children }) => {
       className={`fixed top-0 right-0 h-full bg-gradient-to-b from-gray-800 to-gray-900 shadow-2xl transition-transform transform ${
         isOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
-      style={{ width: '400px', zIndex: 1000 }}
+      style={{ width: '410px', zIndex: 1000 }}
     >
       <div className="text-gray-300 w-[100%]">{children}</div>
     </div>
