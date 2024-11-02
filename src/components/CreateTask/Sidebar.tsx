2b0bb816197ddb4a775da0911449145e1e3837e2
OptimizationGuru <dev.shivamtiwari@gmail.com>
2024-11-02 15:25:50 +0530

    feat:responsive side bar

--- src/components/CreateTask/Sidebar.tsx
+++ src/components/CreateTask/Sidebar.tsx
@@ -11,10 +11,10 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, children }) => {
     <div
       className={`fixed top-0 right-0 h-full bg-gradient-to-b from-gray-800 to-gray-900 shadow-2xl transition-transform transform ${
         isOpen ? 'translate-x-0' : 'translate-x-full'
-      }`}
-      style={{ width: '410px', zIndex: 1000 }}
+      } w-full max-w-[375px] sm:max-w-[410px] md:max-w-[360px] lg:max-w-[350px]`}
+      style={{ zIndex: 1000 }}
     >
-      <div className="text-gray-300 w-[100%]">{children}</div>
+      <div className="text-gray-300 w-full">{children}</div>
     </div>
   );
 };
